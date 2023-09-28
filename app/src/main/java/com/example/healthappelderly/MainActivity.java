2e8dd900365f014d8109d49bb10ec77422b11f6c
Oskar Deubler <deubler.oskar@gmail.com>
2023-09-28 09:24:18 +0200

    phone authetication from two different apps not possible, abandon idea

--- app/src/main/java/com/example/healthappelderly/MainActivity.java
+++ app/src/main/java/com/example/healthappelderly/MainActivity.java
@@ -1,14 +1,82 @@
 package com.example.healthappelderly;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthMissingActivityForRecaptchaException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.ktx.Firebase;
 
 public class MainActivity extends AppCompatActivity {
+    EditText loginCode;
+    Button signupBtn, loginBtn;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signupBtn = findViewById(R.id.signupBtn);
+        loginBtn = findViewById(R.id.loginButton);
+        loginCode = findViewById(R.id.pin);
+        mAuth = FirebaseAuth.getInstance();
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pcode;
+                pcode = String.valueOf(loginCode.getText());
+                if(TextUtils.isEmpty(pcode)){
+                    Toast.makeText(MainActivity.this, "Felaktig kod", Toast.LENGTH_SHORT).show();
+                } else {
+                    verifyCode(pcode);
+                }
+            }
+        });
+
+        signupBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), phoneSignUpTest.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationID, code);
+        signinbyCredentials(credential);
+    }
+
+    private void signinbyCredentials(PhoneAuthCredential credential) {
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
