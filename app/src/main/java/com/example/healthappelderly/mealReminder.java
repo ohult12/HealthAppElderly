bd7d933b8eb72d4c25620c484dc2e872eac84943
Filip Karlsson <filikarl104@student.kau.se>
2023-10-12 09:50:56 +0200

    mealReminder4

--- app/src/main/java/com/example/healthappelderly/mealReminder.java
+++ app/src/main/java/com/example/healthappelderly/mealReminder.java
@@ -7,6 +7,7 @@ import com.google.firebase.auth.FirebaseUser;
 import androidx.annotation.NonNull;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.content.Context;
 import android.widget.ListView;
@@ -19,15 +20,24 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import android.content.SharedPreferences;
 import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class mealReminder extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    DatabaseReference dbTime;
+
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     ListView listView;
+    Button checkBtn;
     String USERNAME_KEY = "ElderApp_Username";
+    private String meal;
+    private String mealTime;
+
 
     @Override
     public void onStart() {
@@ -39,13 +49,18 @@ public class mealReminder extends AppCompatActivity {
         setContentView(R.layout.mealreminder_elderly);
 
         listView = findViewById(R.id.listview);
+        checkBtn = findViewById(R.id.checkBtn);
+        setMeal("Breakfast");
 
-        getDataFromDatabase();
-
-           /* @Override
+        checkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View view) {
-
-        }*/
+                getDataFromDatabase();
+                //change_meal(getTime(), mealTime);
+                String change_meal = change_meal_view(meal);
+                setMeal(change_meal);
+            }
+        });
 
     }
     private String getLocalString(String key) {
@@ -55,27 +70,26 @@ public class mealReminder extends AppCompatActivity {
     //Database
     private void getDataFromDatabase() {
         String username = user.getDisplayName();
-        String date = getDateTime();
+        String date = "2023-10-11"; //getDateTime();
+        String meals = getMeal();
+        String time = getTime();
+        Toast.makeText(mealReminder.this, "Tid: " + time, Toast.LENGTH_SHORT).show();
+
 
         String email = user.getEmail();
-        databaseReference = FirebaseDatabase.getInstance().getReference("Elder").child(username).child("Meals").child(date).child("Lunch");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Elder").child(username).child("Meals").child(date).child(meals);
         ArrayList<String> list = new ArrayList<>();
         ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item, list);
         listView.setAdapter(adapter);
         //Meal meal = new Meal();
 
-
-        /*
-            Skrolldown-menu med olika måltider? Trycka på den måltid man vill åt och lägga till en referens utifrån det. Information kommer upp.
-            Eller bara ha en hel sida med all information från "Meals" i databasen för specifik person.
-        */
-
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 list.clear();
+                list.add(snapshot.getKey().toString());
                 for(DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                        list.add(dataSnapshot.getValue().toString());
+                    list.add(dataSnapshot.getValue().toString());
                 }
                 adapter.notifyDataSetChanged();
             }
@@ -89,5 +103,46 @@ public class mealReminder extends AppCompatActivity {
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
         return dateFormat.format(date);
+    }
+
+    private String getTime() {
+        Date d=new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("hh:mm a");
+        String currentDateTimeString = sdf.format(d);
+        return currentDateTimeString;
+    }
+
+    private void change_meal(String time, String mealtime) {
+        if(time.compareTo(mealtime) > mealtime.compareTo(time)) {
+            change_meal_view(getMeal());
+        } else {
+            Toast.makeText(mealReminder.this, "Time for this meal is not over yet", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    private String change_meal_view(String meal) {
+        switch(meal) {
+            case "Breakfast":
+                meal = "Small meal";
+                break;
+            case "Small meal":
+                meal = "Lunch";
+                break;
+            case "Lunch":
+                meal = "Dinner";
+                break;
+            default:
+                meal = "No more meals today";
+                break;
+        }
+        return meal;
+    }
+
+    public String getMeal() {
+        return meal;
+    }
+    public void setMeal(String meal) {
+        this.meal = meal;
     }
 }
